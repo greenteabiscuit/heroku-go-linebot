92902085bae2516686460fa732deccde48707f6c
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-04-09 18:43:58 +0900

    これで都市2のデータがとれるはず？

--- main.go
+++ main.go
@@ -50,37 +50,50 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 				replyMessage := message.Text
 				res := strings.Split(replyMessage, ",")
 				key := os.Getenv("OPENWEATHER_API_KEY")
-				url := "http://api.openweathermap.org/geo/1.0/direct?q=" + res[0] + "&limit=5&appid=" + key
-				spaceClient := http.Client{
+				url1 := "http://api.openweathermap.org/geo/1.0/direct?q=" + res[0] + "&limit=5&appid=" + key
+				url2 := "http://api.openweathermap.org/geo/1.0/direct?q=" + res[1] + "&limit=5&appid=" + key
+				client1 := http.Client{
 					Timeout: time.Second * 2, // Timeout after 2 seconds
 				}
-				req, err := http.NewRequest(http.MethodGet, url, nil)
-				if err != nil {
-					fmt.Fprintf(w, "bye\n")
+				client2 := http.Client{
+					Timeout: time.Second * 2, // Timeout after 2 seconds
 				}
-				req.Header.Set("User-Agent", "experiment")
+				req1, _ := http.NewRequest(http.MethodGet, url1, nil)
+				req2, _ := http.NewRequest(http.MethodGet, url2, nil)
 
-				httpResponse, getErr := spaceClient.Do(req)
-				if getErr != nil {
-					log.Fatal(getErr)
+				req1.Header.Set("User-Agent", "experiment")
+				req2.Header.Set("User-Agent", "experiment")
+
+				httpResponse1, _ := client1.Do(req1)
+				httpResponse2, _ := client2.Do(req2)
+
+				if httpResponse1.Body != nil {
+					defer httpResponse1.Body.Close()
 				}
 
-				if httpResponse.Body != nil {
-					defer httpResponse.Body.Close()
+				if httpResponse2.Body != nil {
+					defer httpResponse2.Body.Close()
 				}
 
-				body, readErr := ioutil.ReadAll(httpResponse.Body)
-				if readErr != nil {
-					log.Fatal(readErr)
+				body1, _ := ioutil.ReadAll(httpResponse1.Body)
+				body2, _ := ioutil.ReadAll(httpResponse2.Body)
+
+				var gl1 []Geolocation
+				jsonErr1 := json.Unmarshal([]byte(body1), &gl1)
+				if jsonErr1 != nil {
+					log.Fatal(jsonErr1)
 				}
-				var gl []Geolocation
-				jsonErr := json.Unmarshal([]byte(body), &gl)
-				if jsonErr != nil {
-					log.Fatal(jsonErr)
+				var gl2 []Geolocation
+				jsonErr2 := json.Unmarshal([]byte(body2), &gl2)
+				if jsonErr2 != nil {
+					log.Fatal(jsonErr2)
 				}
-				replyMessageLon, replyMessageLat := gl[0].Lon, gl[0].Lat
-				replyMessageLonStr, replyMessageLatStr := strconv.FormatFloat(replyMessageLon, 'f', 2, 64), strconv.FormatFloat(replyMessageLat, 'f', 2, 64)
-				replyMessage = res[0] + "から" + res[1] + "を移動しました。都市1の緯度経度は" + replyMessageLonStr + ", " + replyMessageLatStr + "です。"
+				lon1, lat1 := gl1[0].Lon, gl1[0].Lat
+				lonStr1, latStr1 := strconv.FormatFloat(lon1, 'f', 2, 64), strconv.FormatFloat(lat1, 'f', 2, 64)
+				lon2, lat2 := gl2[0].Lon, gl2[0].Lat
+				lonStr2, latStr2 := strconv.FormatFloat(lon2, 'f', 2, 64), strconv.FormatFloat(lat2, 'f', 2, 64)
+
+				replyMessage = res[0] + "から" + res[1] + "を移動しました。" + res[0] + "の緯度経度は" + lonStr1 + ", " + latStr1 + "です。" + res[1] + "の緯度経度は" + lonStr2 + ", " + latStr2 + "です。"
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
 					log.Print(err)
 				}
